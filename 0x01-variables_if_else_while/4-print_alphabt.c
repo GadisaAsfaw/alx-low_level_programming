329a45ced3a616ef020c13043d607870aa395a8e
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-12 08:45:14 -0500

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print alphabets except q and e
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+		{
+			continue;
+		}
+		putchar (i);
+	}
+	return (0);
+}
