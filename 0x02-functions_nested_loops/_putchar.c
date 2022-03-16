9131c40c5fded63b3507a5faff9f5e251542f285
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-16 08:52:45 -0400

    task-0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *_putchar - prints a char
+ *Return: void
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+/**
+ *This is header file
+ */
+void _putchar(char c);
