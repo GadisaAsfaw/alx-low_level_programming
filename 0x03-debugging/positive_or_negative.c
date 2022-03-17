857ba3f8c56da1bb6a6fd4fefb08a85f728fb099
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-17 08:24:37 -0400

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative -  Entry point
+ *@i: number to be tested
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
