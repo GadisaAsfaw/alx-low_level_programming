c2fbad25fcae1ab4a4a3c00771c72a42dcc8658e
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-18 06:10:43 -0400

    task -6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - draws a straight line
+ *@n: number of times char _ should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
