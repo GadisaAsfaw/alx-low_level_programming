f245e3db1d420c28c2d4721d1452931c38acc2ce
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-21 05:52:19 -0400

    task 3-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strlen - count length of string
+ *@s: char pointer
+ *
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *print_rev - print string in reverse order
+ *@s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
