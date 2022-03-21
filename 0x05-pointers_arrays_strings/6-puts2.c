3c83e09140fc0fa8f7844d6dd1b1df1ae6612c1b
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-21 09:18:51 -0400

    task 6-7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *puts2 - prints every other char
+ *@str: string pointer
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	int cnt = 1;
+
+	while (cnt)
+	{
+		_putchar(*(str + i));
+		if (*(str + i + 1) == '\0' || *(str + i + 2) == '\0')
+		{
+			cnt = 0;
+		}
+		i += 2;
+	}
+	_putchar('\n');
+
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *_strlen - count length of string
+ *@s: char pointer
+ *
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *puts_half - print the 2nd half of string
+ *@str: pointer to string
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int lg = _strlen(str);
+	int hlg = (lg  % 2 == 0) ? lg / 2 : (lg - 1) / 2;
+
+	for (i = hlg; i < lg; i++)
+	{
+		_putchar(*(str + i));
+	}
+
+}
