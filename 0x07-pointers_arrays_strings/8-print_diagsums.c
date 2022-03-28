2ecf8a7cd07a93091a6dba666d59bb477470c972
GadisaAsfaw <gadisasfaw@gmail.com>
2022-03-28 15:05:24 -0400

    task-8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-        int i = 0, sum_1 = 0, sum_2 = 0;
+	int i = 0, sum_1 = 0, sum_2 = 0;
 
-	while(i < size * size)
+	while (i < size * size)
 	{
 		sum_1 += a[i];
-		i += (size+1);
+		i += (size + 1);
 	}
 	i = size - 1;
-	while(i <= size * (size - 1))
+	while (i <= size * (size - 1))
 	{
 		sum_2 += a[i];
 		i += (size - 1);
 	}
-	printf("%d, %d\n",sum_1,sum_2);
+	printf("%d, %d\n", sum_1, sum_2);
 }
