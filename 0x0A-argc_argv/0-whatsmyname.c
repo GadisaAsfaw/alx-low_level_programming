29b8611154365e0873aecb4f0af907d216a1d168
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-01 16:06:38 -0400

    task 0-2

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints its name
+ *@argc:number of argument
+ *@argv: array of strings
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints number of args passed into it
+ *@argc:number of argument
+ *@argv: array of strings
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
+}
