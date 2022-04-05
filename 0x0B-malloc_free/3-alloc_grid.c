b836066b8538a7a5a9db421174dc8b02c4b67d86
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-05 05:27:42 -0400

    free space if malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,19 +11,32 @@
 int **alloc_grid(int width, int height)
 {
 	int **outer;
-	int i, j;
+	int i, j, k;
 
 	if (width == 0 || height == 0)
 		return (NULL);
 
 	outer = (int **)malloc(sizeof(int *) * height);
+
 	if (outer == NULL)
+	{
+		free(outer);
 		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
 		outer[i] = (int *)malloc(sizeof(int) * width);
+
 		if (outer[i] == NULL)
+		{
+			for (k = 0; k <= i; k++)
+				free(outer[i]);
+			free(outer);
 			return (NULL);
+
+		}
+
 		for (j = 0; j < width; j++)
 		{
 			outer[i][j] = 0;
