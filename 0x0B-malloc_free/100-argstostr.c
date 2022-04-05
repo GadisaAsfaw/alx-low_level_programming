8a2ef694bc651f9fcd4464c5f8bf74eb0f1407bd
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-05 12:35:53 -0400

    dbg amount of memory

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,25 +11,20 @@ int _strlen(char *s);
  */
 char *argstostr(int ac, char **av)
 {
-	int *arr_of_strlen;   /* array containing length of each arg/string */
 	char *newstr;         /* pointer to new string*/
 	int i, k, j = 0, newstrlen = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	arr_of_strlen = (int *)malloc(sizeof(int) * ac);
-	if (arr_of_strlen == NULL)
-		return (NULL);
-	/* calculate length of each arg and add to array */
+	/*  calculate length of each arg and add to array */
 	/*  calculate length of new array*/
 	for (i = 0; i < ac; i++)
 	{
-		arr_of_strlen[i] = _strlen(av[i]);
-		newstrlen += arr_of_strlen[i];
+		newstrlen += _strlen(av[i]);
 	}
-	/*for the new line(\n) char and null terminator(\0)*/
-	newstrlen += (ac + 1);
+	/*for the new line(\n)*/
+	newstrlen += (ac - 1);
 
 	/* allocate space for new string */
 	newstr = (char *)malloc(sizeof(char) * newstrlen);
@@ -47,10 +42,12 @@ char *argstostr(int ac, char **av)
 			j++;
 		}
 		/*add new line char to the of each argument/string*/
-		newstr[j] = '\n';
-		j++;
+		if (k < ac - 1)
+		{
+			newstr[j] = '\n';
+			j++;
+		}
 	}
-	newstr[j] = '\0';
 	return (newstr);
 }
 
