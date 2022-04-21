4079842fb4255f1dbe9714e6928c0ac0b47a314a
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-21 10:27:13 -0400

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *list_len - returns number of elements in given linked list
+ *@h: linked list
+ *Return: length of linked list h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
