db6aa6d956b65fd6a981633a8d1a055f8c84b7d2
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-21 09:55:13 -0400

    betty +refactor

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-        if(!h)
-		printf("[0] (nil)");
-	while(h)
+
+	while (h)
 	{
-		printf("[%d] %s\n",h->len,h->str);
+		if (!(h->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
