c62e605b13d639d8c0d68d64028d253af46b7f27
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-26 14:31:10 -0400

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			pre = post;
 			post = post->next;
-			/* idx is greater than or equal to length of the */
-	       	        /* list(its not possible to insert/add) Return NULL*/
+			/*idx is greater than or equal to length of the*/
+			/*list(its not possible to insert/add) Return NULL*/
 			if (post == NULL)
 				return (NULL);
 
