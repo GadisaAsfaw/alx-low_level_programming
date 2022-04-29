57453a0456275691835d73b8ac579a12c2d82c3a
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-29 05:50:26 -0400

    prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,10 @@
 #ifndef BM
 #define BM
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _pow(int b, int p);
 #endif
