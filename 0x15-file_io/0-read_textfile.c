864c9923cca17406455ee3b38d1559644e28aaf0
GadisaAsfaw <gadisasfaw@gmail.com>
2022-05-03 08:02:09 -0400

    refactoring

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,30 +14,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, res_read, res_write;
+	int fd, sz_r, sz_w;
 	char *buf;
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
-	res_read = read(fd, buf, letters);
-	if (res_read == -1)
+	sz_r = read(fd, buf, letters);
+	if (sz_r == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	res_write = write(STDOUT_FILENO, buf, res_read);
-	if (res_write == -1 || res_read != res_write)
+	sz_w = write(STDOUT_FILENO, buf, sz_r);
+	if (sz_w == -1 || sz_r != sz_w)
 	{
 		free(buf);
 		return (0);
 	}
 	free(buf);
 	close(fd);
-	return (res_write);
+	return (sz_r);
 }
